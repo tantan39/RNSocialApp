0c3e509876456eca17e55c139e64afc274f873ed
Tan Tran <tantt.dev@gmail.com>
2025-02-04 22:02:15 -0500

    Install expo-image-picker

---
+++ components/Places/PlacesList.js
@@ -0,0 +1,36 @@
+import { FlatList, View, Text, StyleSheet } from "react-native";
+import PlaceItem from "./PlaceItem";
+import { Colors } from "../../constants/colors";
+
+function PlacesList({ places }) {
+  if (!places || places.length === 0) {
+    return (
+      <View style={styles.fallbackContainer}>
+        <Text style={styles.fallbackText}>No places added yet</Text>
+      </View>
+    );
+  }
+  return (
+    <FlatList
+      data={places}
+      keyExtractor={(item) => {
+        item.id;
+      }}
+      renderItem={({ item }) => <PlaceItem place={item} />}
+    />
+  );
+}
+
+export default PlacesList;
+
+const styles = StyleSheet.create({
+  fallbackContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  fallbackText: {
+    fontSize: 16,
+    color: Colors.primary200,
+  },
+});
